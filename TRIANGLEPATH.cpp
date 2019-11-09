4f72263d4adddb41bc14b285f42633e0fbacf5d1
wjdtjsrms <9kjmse@naver.com>
2019-11-09 16:41:02 +0900

    Test

---
+++ TRIANGLEPATH.cpp
@@ -0,0 +1,26 @@
+// TRIANGLEPATH
+//https://algospot.com/judge/problem/read/TRIANGLEPATH
+#include <iostream>
+
+int main(void){
+
+  int Total = 1;
+  int repeat = 5;
+  int loop = 0;
+  int Test_Value[20]={6,1,2,3,7,4,9,4,1,7,2,7,5,9,4};
+  int i=0;
+  int index=0;
+
+    for(i = 0; i<repeat; i++){
+      for(loop = 0; loop <= i; loop++){
+          std::cout<<Test_Value[index]<<" ";
+          index++;
+      }
+
+      std::cout<<std::endl;
+    }
+
+
+
+  return 0;
+}
